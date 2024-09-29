5fcc207b92c688912d9360cdc49e6cb030ef718a
Viktor Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2024-09-29 19:19:06 +0200

    ADD: working version of validate filename

--- latexgithooks/validate_filename.py
+++ latexgithooks/validate_filename.py
@@ -1,60 +1,10 @@
 __description__ = "Tool for validating TeX filenames meet certain requirements"
 
+import argparse
 import re
-import sys
 import traceback
 from pathlib import Path
-from typing import List, Optional
-
-import typer
-from typing_extensions import Annotated
-
-
-def preprocess_args():
-    """
-    Based on https://github.com/fastapi/typer/issues/554#issuecomment-2323176039
-    Typer doesn't support whitespace-separated multi-value options.
-
-    We preprocess the sysargv so that:
-    - python3 app.py some_command --filters filter1 filter2 filter3 --environments env1 env2 env3
-
-    becomes:
-    - python3 app.py some_command --filters filter1 --filters filter2 --filters filter3 --environments env1 --environments env2 --environments env3
-
-    //!\\ DOWNSIDE: options should always be after arguments in the CLI command //!\\
-    """
-    print("-" * 30, "\n")
-    # get main cmd
-    final_cmd = []
-    for idx, arg in enumerate(sys.argv):
-        if any(arg.startswith(_) for _ in ["-", "--"]):
-            break
-        else:
-            final_cmd.append(arg)
-    print(f"Main command is: {final_cmd}")
-
-    # get options and their values
-    for idx, arg in enumerate(sys.argv):
-        if any(arg.startswith(_) for _ in ["-", "--"]):
-            opt_values = []
-            for value in sys.argv[idx + 1 :]:
-                if any(value.startswith(_) for _ in ["-", "--"]):
-                    break
-                else:
-                    opt_values.append(value)
-
-            if len(opt_values) >= 1:
-                [final_cmd.extend([arg, opt_value]) for opt_value in opt_values]
-            else:
-                final_cmd.append(arg)
-
-    # replace by reformatted
-    print(f"Final command is: {final_cmd}")
-    sys.argv = final_cmd
-
-
-preprocess_args()
-app = typer.Typer(help=__description__)
+from typing import Sequence
 
 
 def has_mixed_case(input_string: str) -> bool:
@@ -73,45 +23,68 @@ def has_mixed_case(input_string: str) -> bool:
     return has_uppercase and has_lowercase
 
 
-@app.command()
-def validate(
-    min_len: Annotated[
-        Optional[int],
-        typer.Option(prompt=False, help="Minimum length for a filename."),
-    ] = 4,
-    verbose: Annotated[
-        Optional[bool],
-        typer.Option(prompt=False, help="Prints additional information"),
-    ] = True,
-    filenames: List[str] = typer.Option([], help="List of filenames to validate"),
-) -> None:
-    """
-    Method for validating TeX filenames meet certain requirements.
+def is_valid_filename(filename: str, min_len: int = 4, verbose: bool = False) -> bool:
+    """Method for validating TeX filename meet certain requirements.
+
+    Args:
+        filename (str): name of the file
+        min_len (int): min lne
+        verbose (bool, optional): prints additional information. Defaults to False.
+
+    Returns:
+        bool: valid or invalid file name
     """
 
-    # SNAKE_CASE_REGEX = re.compile(r"^[a-z_]+$")
     TEX_FILENAME_REGEX = re.compile("^[a-zA-Z0-9_.-]+$")
 
-    for filename in filenames:
-        try:
-            name = Path(filename).stem
-            if verbose:
-                typer.echo(f"Processing filename: {filename}", err=False)
+    try:
+        name = Path(filename).stem
+        if verbose:
+            print(f"Processing filename: {filename}")
 
-            if len(name) < min_len:
-                typer.echo(f"Name too short ({min_len=}): {filename}", err=True)
+        if len(name) < min_len:
+            print(f"Name too short ({min_len=}): {filename}")
+            return False
 
-            if not TEX_FILENAME_REGEX.match(name):
-                typer.echo(f"TeX filename is not valid: {filename}", err=True)
+        if not TEX_FILENAME_REGEX.match(name):
+            print(f"TeX filename is not valid: {filename}")
+            return False
 
-            if has_mixed_case(filename[1:]):
-                typer.echo(f"TeX filename should not contain mixed cases: {filename}", err=True)
+        if has_mixed_case(filename[1:]):
+            print(f"TeX filename should not contain mixed cases: {filename}")
+            return False
 
-        except Exception:
-            # if verbose:
-            typer.echo(f"Filename: {filenames}; exception: {traceback.format_exc()}", err=True)
+    except Exception:
+        # if verbose:
+        print(f"Filename: {filename}; exception: {traceback.format_exc()}")
+        return False
+
+    return True
 
 
-if __name__ == "__main__":
-    preprocess_args()
-    app()
+def main(argv: Sequence[str] | None = None) -> int:
+
+    parser = argparse.ArgumentParser(prog="validate-filename", description=__description__)
+    parser.add_argument(
+        "filenames",
+        nargs="*",
+        help="Filenames to process.",
+    )
+    parser.add_argument(
+        "--min-len",
+        default=4,
+        type=int,
+        help="Minimum length for a filename.",
+    )
+
+    parser.add_argument(
+        "--verbose",
+        default=False,
+        type=bool,
+        help="Prints additional information.",
+    )
+
+    args = parser.parse_args(argv)
+
+    results = [not is_valid_filename(filename, args.min_len, args.verbose) for filename in args.filenames]
+    return int(any(results))
--- tests/test_validate_filename.py
+++ tests/test_validate_filename.py
@@ -1,34 +1,10 @@
 import os
-from typing import Any
 
 import pytest
-from typer.testing import CliRunner
 
-import latexgithooks as latexgithooks
-import latexgithooks.validate_filename as validate_filename
-from latexgithooks.validate_filename import app
+from latexgithooks.validate_filename import has_mixed_case, is_valid_filename, main
 
-runner = CliRunner()
-
-
-def test_has_mixed_case_true():
-    assert validate_filename.has_mixed_case("Hello, World!") == True
-
-
-def test_has_mixed_case_false_uppercase_only():
-    assert validate_filename.has_mixed_case("HELLO") == False
-
-
-def test_has_mixed_case_false_lowercase_only():
-    assert validate_filename.has_mixed_case("hello") == False
-
-
-def test_has_mixed_case_false_empty_string():
-    assert validate_filename.has_mixed_case("") == False
-
-
-def test_has_mixed_case_false_special_characters():
-    assert validate_filename.has_mixed_case("!@#$%^&*()") == False
+# Fixtures
 
 
 @pytest.fixture
@@ -53,12 +29,54 @@ def tex_file_path(folder_name="data", file_pattern=".tex"):
     return file_paths
 
 
-def test_cli_invoke_validate_filenames(tex_file_path):
+# Test has_mixed_case function
 
-    tex_files = tex_file_path
-    cli_params = []
-    for file in tex_files:
-        cli_params.append("--filenames")
-        cli_params.append(file)
-    result = runner.invoke(app, cli_params)
-    assert result.exit_code == 0
+
+def test_has_mixed_case_true():
+    assert has_mixed_case("Hello, World!") == True
+
+
+def test_has_mixed_case_false_uppercase_only():
+    assert has_mixed_case("HELLO") == False
+
+
+def test_has_mixed_case_false_lowercase_only():
+    assert has_mixed_case("hello") == False
+
+
+def test_has_mixed_case_false_empty_string():
+    assert has_mixed_case("") == False
+
+
+def test_has_mixed_case_false_special_characters():
+    assert has_mixed_case("!@#$%^&*()") == False
+
+
+# Test cases for `is_valid_filename` function
+def test_is_valid_filename_valid():
+    assert is_valid_filename("valid_filename.tex", 3)
+
+
+def test_is_valid_filename_too_short():
+    assert not is_valid_filename("a.tex", 3)
+
+
+def test_is_valid_filename_invalid_characters():
+    assert not is_valid_filename("invalid_filename#$.tex", 3)
+
+
+def test_is_valid_filename_mixed_case():
+    assert not is_valid_filename("MixedCaseFilename.tex", 3)
+
+
+def test_is_valid_filename_verbose_mode():
+    assert not is_valid_filename("MixedCaseFilename.tex", 3, verbose=True)
+
+
+# Test cases for `main` function
+def test_main_valid_filenames():
+    assert main(["valid_filename.tex", "another_valid_filename.tex"]) == 0
+
+
+def test_main_invalid_filenames():
+    assert main(["invalid_filename&.tex", "tex"]) == 1
